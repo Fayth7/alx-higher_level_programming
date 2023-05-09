4d07784001cfb0ccc445a3d1c8cd2d8378afa41b
Fayth7 <flexflakes@gmail.com>
2023-05-09 09:59:42 -0700

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 8-uppercase.py
+def uppercase(str):
+    for char in str:
+        uppercase_char = chr(ord(char) - 32) if ord(char) >= 97 and ord(char) <= 122 else char
+        print(uppercase_char, end='')
+    print()
