e26839500fcad9e751c1a7c1f2c38fc1094112f2
Fayth7 <flexflakes@gmail.com>
2023-05-09 10:21:30 -0700

    pow

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 # 8-uppercase.py
 def uppercase(str):
     for char in str:
-        uppercase_char = chr(ord(char) - 32)
-        if ord(char) >= 97 and ord(char) <= 122 else char
-        print("{}".format(uppercase_char), end='')
-    print()
+        if ord(char) >= 97 and ord(char) <= 122
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # 9-print_last_digit.py
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    return (last_digit)
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
