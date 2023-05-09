eb14f78b0eb05c9188b5ca1fde4b2a307ddba6be
Fayth7 <flexflakes@gmail.com>
2023-05-09 09:53:53 -0700

    is lower

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-for i in range(10):
+# 6-print_comb3.py
+for i in range(0,10):
     for j in range(i + 1, 10):
-        print("{:02d}".format(i), end="")
-        print("{:02d}".format(j), end="")
-        if i != 8 or j != 9:
-            print(", ", end="")
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
         else:
-            print()
+            print("{}{}".format(i, j), end=", ")
             
