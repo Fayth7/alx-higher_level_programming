af4175aad29eb75ee9193d7d6e21961ba3b6a3e1
Fayth7 <flexflakes@gmail.com>
2023-05-09 15:40:33 -0700

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # 100-print_tebahpla.py
-for i in range(ord('z'), ord('a') - 1, -1):
-    letter = chr(i)
-    if i % 2 == 0:
-        print(letter.upper(), end="")
-    else:
-        print(letter.lower(), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
