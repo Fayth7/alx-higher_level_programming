64c70eb0bae3c9bad408c62ce39e303cd9aee9d8
Fayth7 <flexflakes@gmail.com>
2023-05-11 12:45:25 -0700

    correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add = __import__('magic_calculation_102').add
-    sub = __import__('magic_calculation_102').sub
+    """Match bytecode provided by Holberton School."""
+    def add(a, b):
+        return a + b
+
+    def sub(a, b):
+        return a - b
 
     if a < b:
         c = add(a, b)
-        for i in range(4, 7):
+        for i in range(4, 6):
             c = add(c, i)
         return c
     else:
