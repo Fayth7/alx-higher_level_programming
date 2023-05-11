e91d6d8ccfb6ea2bb8460a303c41d41fb367c447
Fayth7 <flexflakes@gmail.com>
2023-05-11 12:43:42 -0700

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-arguments = sys.argv[1:]
-num_arguments = len(arguments)
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
 
-print("{} argument{}:".format(num_arguments, "s" if num_arguments != 1 else ""))
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
 
-if num_arguments == 0:
-    print("0 arguments.")
-else:
-    for i, argument in enumerate(arguments, start=1):
-        print("{}: {}".format(i, argument))
+    for i in range(1, num_args + 1):
+        print(f"{i}: {sys.argv[i]}")
