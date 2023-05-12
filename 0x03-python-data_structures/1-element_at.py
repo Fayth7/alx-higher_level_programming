8bccc690f8ce349d2fec80e3ece3b17ac52ecc4e
Fayth7 <flexflakes@gmail.com>
2023-05-12 06:04:55 -0700

    replace list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-element_at.py
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+
+    i = 0
+    while i <= idx:
+        if i == idx:
+            return my_list[i]
+        i += 1
