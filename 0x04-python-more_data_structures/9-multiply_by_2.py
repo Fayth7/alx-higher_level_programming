0f22bf4acaade633e35184975c7f3f5435ba6e4a
Fayth7 <flexflakes@gmail.com>
2023-05-18 06:26:06 -0700

    roman

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     multiplied_dictionary = {}
     for key, value in a_dictionary.items():
         multiplied_dictionary[key] = value * 2
-    return( multiplied_dictionary)
+    return(multiplied_dictionary)
