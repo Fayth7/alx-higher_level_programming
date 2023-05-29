48ec54cf5af9e5b8da05752be258cffc87479e30
Fayth7 <flexflakes@gmailcom>
2023-05-29 12:48:21 +0000

    exceptions

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,58 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p)
+{
+  Py_ssize_t size, i;
+  PyObject *item;
+
+  if (!PyList_Check(p))
+    {
+      printf("[ERROR] Invalid List Object\n");
+      return;
+    }
+
+  size = PyList_Size(p);
+  printf("[*] Python list info\n[*] Size of the Python List = %ld\n[*] Allocated = %ld\n", size, ((PyListObject *)p)->allocated);
+
+  for (i = 0; i < size; i++)
+    {
+      item = PyList_GetItem(p, i);
+      printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
+
+void print_python_bytes(PyObject *p)
+{
+  Py_ssize_t size i;
+  char *bytes_str;
+
+  if (!PyBytes_Check(p))
+    {
+      printf("[ERROR] Invalid Bytes Object\n");
+      return;
+    }
+
+  size = PyBytes_Size(p);
+  bytes_str = PyBytes_AsString(p);
+
+  printf("[.] bytes object info\n  size: %ld\n  trying string: %s\n", size, bytes_str);
+
+  printf("first %d bytes:", (size < 10) ? (int)size : 10);
+  for (i = 0; i < size && i < 10; i++)
+    printf(" %.2x", (unsigned char)bytes_str[i]);
+  printf("\n");
+}
+
+void print_python_float(PyObject *p)
+{
+  double value;
+
+  if (!PyFloat_Check(p))
+    {
+      printf("[ERROR] Invalid Float Object\n");
+      return;
+    }
+
+  value = PyFloat_AS_DOUBLE(p);
+  printf("[.] float object info\n  value: %f\n", value);
+}
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            quotient = 0
+            if i < len(my_list_1) and i < len(my_list_2):
+                dividend = my_list_1[i]
+                divisor = my_list_2[i]
+                if isinstance(dividend, (int, float)) and (
+                        isinstance(divisor, (int, float))):
+                    if divisor != 0:
+                        quotient = dividend / divisor
+                    else:
+                        print("division by 0")
+                else:
+                    print("wrong type")
+            else:
+                print("out of range")
+            result.append(quotient)
+        except Exception as e:
+            result.append(0)
+    return (result)
