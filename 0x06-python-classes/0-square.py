3126c2fc16d28343c46760824cee62ebd0e78e38
Fayth7 <flexflakes@gmailcom>
2023-05-30 12:15:01 +0000

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-class Square:
-  """
-    Square class represents a square.
+"""Define a class Square."""
 
-    Attributes:
-        __size (float or int): The size of the square.
-    """
+
+class Square:
+
+    """Square class represents a square."""
     pass
