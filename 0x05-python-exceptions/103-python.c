0740b6ef1ec4dca6483fc0d746b9e78c079d3d38
Fayth7 <flexflakes@gmailcom>
2023-05-30 14:57:04 +0000

    corrections 2

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -11,13 +11,17 @@ void print_python_list(PyObject *p)
       return;
     }
 
-  size = PyList_Size(p);
+  size = 0;
+  while (PyObject_GetItem(p, PyLong_FromSsize_t(size)) != NULL)
+    size++;
+
   printf("[*] Python list info\n[*] Size of the Python List = %zd\n[*] Allocated = %zd\n", size, ((PyListObject *)p)->allocated);
 
   for (i = 0; i < size; i++)
     {
-      item = PyList_GetItem(p, i);
+      item = PyObject_GetItem(p, PyLong_FromSsize_t(i));
       printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+      Py_DECREF(item);
     }
 }
 
@@ -32,8 +36,8 @@ void print_python_bytes(PyObject *p)
       return;
     }
 
-  size = PyBytes_Size(p);
-  bytes_str = PyBytes_AsString(p);
+  size = PyObject_Length(p);
+  bytes_str = PyBytes_AsString(PyObject_Str(p));
 
   printf("[.] bytes object info\n  size: %zd\n  trying string: %s\n", size, bytes_str);
 
@@ -53,6 +57,68 @@ void print_python_float(PyObject *p)
       return;
     }
 
-  value = PyFloat_AS_DOUBLE(p);
+  value = PyFloat_AsDouble(p);
+  printf("[.] float object info\n  value: %f\n", value);
+}
+#include <Python.h>
+
+void print_python_list(PyObject *p)
+{
+  Py_ssize_t size, i;
+  PyObject *item;
+
+  if (!PyList_Check(p))
+    {
+      printf("[ERROR] Invalid List Object\n");
+      return;
+    }
+
+  size = 0;
+  while (PyObject_GetItem(p, PyLong_FromSsize_t(size)) != NULL)
+    size++;
+
+  printf("[*] Python list info\n[*] Size of the Python List = %zd\n[*] Allocated = %zd\n", size, ((PyListObject *)p)->allocated);
+
+  for (i = 0; i < size; i++)
+    {
+      item = PyObject_GetItem(p, PyLong_FromSsize_t(i));
+      printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+      Py_DECREF(item);
+    }
+}
+
+void print_python_bytes(PyObject *p)
+{
+  Py_ssize_t size, i;
+  char *bytes_str;
+
+  if (!PyBytes_Check(p))
+    {
+      printf("[ERROR] Invalid Bytes Object\n");
+      return;
+    }
+
+  size = PyObject_Length(p);
+  bytes_str = PyBytes_AsString(PyObject_Str(p));
+
+  printf("[.] bytes object info\n  size: %zd\n  trying string: %s\n", size, bytes_str);
+
+  printf("first %d bytes:", (int)((size < 10) ? size : 10));
+  for (i = 0; i < size && i < 10; i++)
+    printf(" %.2x", (unsigned char)bytes_str[i]);
+  printf("\n");
+}
+
+void print_python_float(PyObject *p)
+{
+  double value;
+
+  if (!PyFloat_Check(p))
+    {
+      printf("[ERROR] Invalid Float Object\n");
+      return;
+    }
+
+  value = PyFloat_AsDouble(p);
   printf("[.] float object info\n  value: %f\n", value);
 }
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -21,6 +21,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except Exception as e:
             result.append(0)
         finally:
-            # Code to be executed regardless of whether an exception occurs or not
+            # Code to be executed whether an exception occurs or not
             pass
     return (result)
