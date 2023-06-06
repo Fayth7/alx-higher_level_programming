e517981930c59df7ce664bccab12030ad8998f07
Fayth7 <flexflakes@gmailcom>
2023-06-06 12:45:06 +0000

    updates

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""returns a sting n times the number of iteration"""
-
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
     return "BestSchool" + ", BestSchool" * (magic_string.count - 1)
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-""" prevents the user from dynamically creating new instance attributes"""
-
 class LockedClass:
     __slots__ = ["first_name"]
