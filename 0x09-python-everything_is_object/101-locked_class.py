566dfea44ed86840f62d84206e62cfe3558047ec
Fayth7 <flexflakes@gmailcom>
2023-06-07 13:47:18 +0000

    line 1

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
+"""
+Module that defines the LockedClass.
+
+The LockedClass is a class with restricted attribute assignment.
+"""
+
 class LockedClass:
     __slots__ = ["first_name"]
