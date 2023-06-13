74a075455bf30ce562ef2684c683f50f608bb9c5
Fayth7 <flexflakes@gmailcom>
2023-06-13 16:52:24 +0000

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,20 +3,18 @@
 Script to add arguments to a Python list and save them to a file.
 """
 import sys
-from os.path import exists
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+import json
+import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-# Create an empty list or load existing list from file
-if exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
 
-# Add arguments to the list
-my_list.extend(sys.argv[1:])
+items.extend(sys.argv[1:])
 
-# Save the updated list to the file
-save_to_json_file(my_list, filename)
+save_to_json_file(items, filename)
