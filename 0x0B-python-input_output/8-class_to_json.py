ec939d2abc83d66fc80f3f96567fe6297a7aa93e
Fayth7 <flexflakes@gmailcom>
2023-06-13 16:57:55 +0000

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,15 +1,9 @@
 def class_to_json(obj):
-    """Converts an object instance to a dictionary representation for JSON serialization."""
-    json_dict = {}
+    """ returns dictionary description with simple data structure for JSON."""
 
-    # Iterate over the object's attributes
-    for attr in dir(obj):
-        # Exclude private and special attributes
-        if not attr.startswith("__"):
-            value = getattr(obj, attr)
 
-            # Check if the attribute value is of a serializable type
-            if isinstance(value, (list, dict, str, int, bool)):
-                json_dict[attr] = value
-
-    return json_dict
+def class_to_json(obj):
+    """
+        returns dictionary representation with simple data structure.
+    """
+    return obj.__dict__
