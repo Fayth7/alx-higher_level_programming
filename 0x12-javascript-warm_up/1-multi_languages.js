4301e0af72c88a428f0b6af653bd6a42dcabcf7f
Fayth7 <flexflakes@gmail.com>
2023-08-14 14:54:42 +0300

    task 3

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let firstline = 'C is fun';
+console.log('C is fun');
+
+let secondLine = 'Python is cool';
+console.log('Python is cool');
+
+let thirdLine = 'JavaScript is amazing';
+console.log('JavaScript is amazing');
