e2447d16bfea3e94db34cc3f238dbc5646ab1dc9
Fayth7 <flexflakes@gmail.com>
2023-08-18 15:30:14 +0300

    task 1 correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,35 +1,16 @@
 #!/usr/bin/python3
 """
-Lists states with a name starting with 'N' from the database hbtn_0e_0_usa.
+Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+Usage: ./1-filter_states.py <mysql username> \
+                             <mysql password> \
+                             <database name>
 """
-
 import sys
 import MySQLdb
 
-
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306,
-                             user=username, passwd=password, db=database)
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER id ASC")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-        cur.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        sys.exit(1)
-
-
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY `id`")
+    for state in c.fetchall():
+        print(state)
