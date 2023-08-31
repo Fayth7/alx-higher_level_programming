eed877ca0fa19e241806d0f32b65d0313c5acb7b
Fayth7 <flexflakes@gmail.com>
2023-08-31 19:10:50 +0300

    task 3 correction

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes URL, sends a GET request to URL, and displays body of response
-curl -sfL "$1" -X GET
+curl -s -X GET "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument and displays all HTTP methods the server will accept
-curl -s -i -X OPTIONS "$1" | grep -i Allow | cut -d ' ' -f2-
+curl -sI "$1" | grep "Allow" | cut -d ' ' -f 2-
