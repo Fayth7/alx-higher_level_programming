ced3ee57588fa58aecf4c12af506739182f36765
Fayth7 <flexflakes@gmail.com>
2023-09-01 14:03:20 +0300

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -16,6 +16,7 @@ If the list is empty, the function returns None.
 
 
 def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
 
